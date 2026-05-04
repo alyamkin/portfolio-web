506105dddcc4a1356cfc229926c7ee62a74eca52
Andrey Lyamkin <andrew.lyamkin@gmail.com>
2026-05-04 15:38:24 -0400

    Init project structure

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import '@/styles/globals.css';
+import type { Metadata } from 'next';
+import { inter } from '@/styles/fonts';
+
+export const metadata: Metadata = {
+  title: 'Andrey Lyamkin | Portfolio',
+  description: 'Andrey Lyamkin portfolio website',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable}`}>
+      <body className="font-inter"> {children}</body>
+    </html>
+  );
+}
---
+++ styles/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Roboto } from 'next/font/google';
+
+export const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
